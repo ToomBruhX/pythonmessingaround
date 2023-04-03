fb913e1fd3293e67e85285c230b2fa0d01e6c524
Tom Lifshitz <toombruhx@Toms-MBP.lan1>
2023-04-03 08:43:34 -0700

    Quick change

--- Day1.py
+++ Day1.py
@@ -2,6 +2,7 @@ robin = "frodo"
 tom = "valorant"
 tomkd = 4000
 frodobite = 1000000000
+truth = "Robin is actually better then me at valorant"
 
 print("The best LOTR character is:" + robin)
 print(frodobite)
